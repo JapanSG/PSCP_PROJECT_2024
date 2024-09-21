b04b14cc163c3d64f82fe205a7f43ab4d0e382ea
JapanSG <japan.sri45326@gmail.com>
2024-09-21 22:03:54 +0700

    Add and modify
    changed old save.py to save_gui.py
    added new save.py as a module for editing and reading file
    added password.txt for storing passwords

--- PSCP-PROJECT/save.py
+++ PSCP-PROJECT/save.py
@@ -1,60 +1,16 @@
-import tkinter as tk
-from tkinter import *
-
-def save():
-    '''Save password to a file(not finished)'''
-    global username_field, password_field
-    username = username_field.get()
-    password = password_field.get() 
-    print(username)
-    print(password)
-
-master = tk.Tk(className="Password-saver")
-master.geometry("500x500")
-
-username_title = Label(
-    master,
-    text = "username",
-    font = ("Arial",10)
-    )
-
-password_title = Label(
-    master,
-    text = "password",
-    font = ("Arial",10)
-    )
-
-username_field = Entry(
-    master,
-    text = "username",
-    font = ("Arial",10),
-    width = 20
-    )
-
-password_field = Entry(
-    master,
-    text = "password",
-    font = ("Arial",10),
-    width = 20,
-    show="*"
-    )
-
-save_button = Button(
-    master,
-    text = "save",
-    command = save
-    )
-
-username_title.grid(row = 0, column = 0, pady = 10)
-username_field.grid(row = 0, column = 1, pady = 10)
-password_title.grid(row = 1, column = 0, pady = 10)
-password_field.grid(row = 1, column = 1, pady = 10)
-save_button.grid(row = 2, column = 1, sticky = 'E',pady = 10)
-
+'''save'''
+def add_password(username : str, password : str) -> None:
+    '''Add username and password to a file'''
+    with open("password.txt","a") as file:
+        file.write(f"username: {username}\n")
+        file.write(f"password: {password}\n")
+def clear_file() -> None:
+    '''Clear all content in a file'''
+    open("password.txt","w").close()
 def main():
     '''Driver Code'''
-    global master
-    master.mainloop()
-
+    username = input()
+    password = input()
+    add_password(username,password)
 if __name__ == "__main__":
-    main()
+    clear_file()
---
+++ PSCP-PROJECT/save_gui.py
@@ -0,0 +1,61 @@
+'''save_gui'''
+import tkinter as tk
+from tkinter import *
+
+def save():
+    '''Save password to a file(not finished)'''
+    global username_field, password_field
+    username = username_field.get()
+    password = password_field.get() 
+    print(username)
+    print(password)
+
+master = tk.Tk(className="Password-saver")
+master.geometry("500x500")
+
+username_title = Label(
+    master,
+    text = "username",
+    font = ("Arial",10)
+    )
+
+password_title = Label(
+    master,
+    text = "password",
+    font = ("Arial",10)
+    )
+
+username_field = Entry(
+    master,
+    text = "username",
+    font = ("Arial",10),
+    width = 20
+    )
+
+password_field = Entry(
+    master,
+    text = "password",
+    font = ("Arial",10),
+    width = 20,
+    show="*"
+    )
+
+save_button = Button(
+    master,
+    text = "save",
+    command = save
+    )
+
+username_title.grid(row = 0, column = 0, pady = 10)
+username_field.grid(row = 0, column = 1, pady = 10)
+password_title.grid(row = 1, column = 0, pady = 10)
+password_field.grid(row = 1, column = 1, pady = 10)
+save_button.grid(row = 2, column = 1, sticky = 'E',pady = 10)
+
+def main():
+    '''Driver Code'''
+    global master
+    master.mainloop()
+
+if __name__ == "__main__":
+    main()
