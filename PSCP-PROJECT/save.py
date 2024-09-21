59e8aaa1bfde075cec839d0c49ec5fefc59e35cf
JapanSG <japan.sri45326@gmail.com>
2024-09-21 22:45:24 +0700

    Added more functionality to save module

--- PSCP-PROJECT/save.py
+++ PSCP-PROJECT/save.py
@@ -7,10 +7,26 @@ def add_password(username : str, password : str) -> None:
 def clear_file() -> None:
     '''Clear all content in a file'''
     open("password.txt","w").close()
+def read_file() -> None:
+    '''Read all content in a file'''
+    with open("password.txt","r") as file:
+        lines = file.readlines()
+        num = 1
+        for i in range(0,len(lines),2):
+            print(num)
+            print(lines[i],lines[i+1],sep = "")
+            num += 1
+def get_pass(line_num:int)->str:
+    '''Return username and password at a specific line'''
+    with open("password.txt","r") as file:
+        lines = file.readlines()
+        line_num = line_num*2-2
+        return f"{lines[line_num]}{lines[line_num+1]}"
 def main():
     '''Driver Code'''
     username = input()
     password = input()
     add_password(username,password)
+    read_file()
 if __name__ == "__main__":
-    clear_file()
+    main()
