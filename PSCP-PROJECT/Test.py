b2fee9bd7d5ab769b3813edcb12e0e7895eed391
Chisanupong22 <67070220@kmitl.ac.th>
2024-09-22 20:37:04 +0700

    ADD PreChooseButton

--- PSCP-PROJECT/Test.py
+++ PSCP-PROJECT/Test.py
@@ -1,48 +1,39 @@
+import customtkinter, tkinter
+from tkinter import *
 import string
 import random
-"""choose"""
-char = ''
-letters = False
-digits = False
-punctuation = False
-lenght = int(input('How long? '))
-l = input('Do you want letters: ')#จริงๆก็เป็นการติ้กแหละ
-d = input('Do you want digits: ')#
-p = input('Do you want punctuation: ')#
-if l =='Yes':
-    letters = True
-
-if letters == True:#กดติ้ก
-    only_lower = 0
-    only_upper = 0
-    up = input('Letters Up? ')
-    lo = input('Letters Lower? ')
-    if up == '1':#กดติ้ก
-        only_upper+=1
-        char+=string.ascii_uppercase
-    elif up == '0':#ไม่กดติ้ก
-        char = ''
-    
-    if lo == '1':#กดติ้ก
-        only_lower+=1
-        char+=string.ascii_lowercase
-    elif lo == '0':#ไม่กดติ้ก
-        char = ''
-
-if d == 'Yes':#กดติ้ก
-    digits = True
-    
-if digits == True:
-    char+=string.digits
-
-if p == 'Yes':
-    punctuation = True
-
-if punctuation == True:
-    char+=string.punctuation
-    
-password = []
-for i in range(lenght):
-    randoms = random.choice(char)
-    password.append(randoms)
-print(''.join(password))
+a = customtkinter.CTk()
+a.title("TEST")
+a.geometry("500x500")
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
+def check():
+    charlist = ''
+    if check_upper.get() == 'on':
+        charlist+=string.ascii_uppercase
+    if check_lower.get() == 'on':
+        charlist+=string.ascii_lowercase
+    if check_digits.get() == 'on':
+        charlist+=string.digits
+    if check_punctuation.get() == 'on':
+        charlist+=string.punctuation
+    text_result.set(charlist)
+check_upper = customtkinter.StringVar(value="off")
+check_lower = customtkinter.StringVar(value="off")
+check_digits = customtkinter.StringVar(value="off")
+check_punctuation = customtkinter.StringVar(value="off")
+upper = customtkinter.CTkCheckBox(a,text="Uppercase",command=check,variable=check_upper,onvalue="on",offvalue="off")
+lower = customtkinter.CTkCheckBox(a,text="Lowercase",command=check,variable=check_lower,onvalue="on",offvalue="off")
+digits = customtkinter.CTkCheckBox(a,text="Digits",command=check,variable=check_digits,onvalue="on",offvalue="off")
+punctuation = customtkinter.CTkCheckBox(a,text="Punctuation",command=check,variable=check_punctuation,onvalue="on",offvalue="off")
+upper.place(relx=0.55,rely=0.4,anchor=tkinter.CENTER)
+lower.place(relx=0.55,rely=0.5,anchor=tkinter.CENTER)
+digits.place(relx=0.55,rely=0.6,anchor=tkinter.CENTER)
+punctuation.place(relx=0.55,rely=0.7,anchor=tkinter.CENTER)
+button = customtkinter.CTkButton(master=a,width=120,height=32,border_width=0,corner_radius=8,text="print")
+button.place(relx=0.5,rely=0.8,anchor=tkinter.CENTER)
+text_result = tkinter.StringVar(value="")
+label_result = customtkinter.CTkLabel(master=a,textvariable=text_result,
+                                        width=220,height=60,text_color="black",font=("Arial",25),fg_color=("blue"))
+label_result.place(relx=0.55,rely=0.9,anchor=tkinter.CENTER)
+a.mainloop()
