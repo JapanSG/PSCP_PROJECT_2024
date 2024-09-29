01dfaf874f4462f8b3647b681884214543fd5139
Chisanupong22 <67070220@kmitl.ac.th>
2024-09-29 19:12:15 +0700

    Add Slider to set lenght of the Password and rename function that name dupe

--- PSCP-PROJECT/Test.py
+++ PSCP-PROJECT/Test.py
@@ -21,11 +21,15 @@ def check():
 
 def showed():
     charlist = check()
-    password = []
-    for i in range(5):
-        randoms = random.choice(charlist)
-        password.append(randoms)
-    text_result.set(''.join(password))
+    if charlist:
+        password_length = int(slider.get())
+        password = []
+        for _ in range(password_length):
+            randoms = random.choice(charlist)
+            password.append(randoms)
+        text_result.set(''.join(password))
+    else:
+        text_result.set("No character set selected!")
     
 check_upper = customtkinter.StringVar(value="off")
 check_lower = customtkinter.StringVar(value="off")
@@ -47,15 +51,28 @@ label_result = customtkinter.CTkLabel(master=a,textvariable=text_result,
                                         width=220,height=60,text_color="black",font=("Arial",25),fg_color=("blue"))
 label_result.place(relx=0.55,rely=0.9,anchor=tkinter.CENTER)
 #ลองสร้างปิด/เปิดตา
-def check():
+def toggle_entry_visibility():
     if check_test.get() == 'on':
         entry.config(show='')
     else:
         entry.config(show='*')
+
 check_test = customtkinter.StringVar(value='off')
 entry = Entry(a,show='*')
 entry.place(relx=0.55,rely=0.1)
+check_test = customtkinter.StringVar(value='off')
 check_pass = customtkinter.CTkCheckBox(a,text="show",command=check,variable=check_test,onvalue='on',offvalue='off')
 check_pass.place(relx=0.55,rely=0.2)
+#ลองสร้างslide
+def update_length_label(value):
+    length_label_var.set(f"Length: {int(float(value))}")
 
+length_label_var = tkinter.StringVar(value="Length: 8")
+length_label = customtkinter.CTkLabel(a, textvariable=length_label_var, font=("Arial", 14))
+length_label.place(relx=0.5, rely=0.78, anchor=tkinter.CENTER)
+slider_label = customtkinter.CTkLabel(a, text="Password Length", font=("Arial", 16))
+slider_label.place(relx=0.5, rely=0.1, anchor=tkinter.CENTER)
+slider = customtkinter.CTkSlider(master=a, from_=4, to=20, number_of_steps=16, command=update_length_label)
+slider.set(8)  # Default ขนาด
+slider.place(relx=0.5, rely=0.75, anchor=tkinter.CENTER)
 a.mainloop()
