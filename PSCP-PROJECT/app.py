b23e89fc821a70a3d9ee4355de8106b2afe24960
JapanSG <japan.sri45326@gmail.com>
2024-10-05 11:36:09 +0700

    Modified app.py Added title_frame

--- PSCP-PROJECT/app.py
+++ PSCP-PROJECT/app.py
@@ -3,12 +3,16 @@ from tkinter import *
 from customtkinter import *
 from PIL import Image
 APP_FONT = ("K2D",24)
+WHITE = "#E3F1FB"
+BLUE = "#1650CC"
+DARK_BLUE = "#17377D"
+LIGHT_BLUE = "#8AA7E6"
 
 class App(CTk):
     '''App Class'''
     def __init__(self):
         '''Contructor'''
-        super().__init__(fg_color="#E3F1FB")
+        super().__init__(fg_color = WHITE)
 
         self.geometry("1024x768")
         self.title("Password Generator")
@@ -23,57 +27,60 @@ class NavBar(CTkFrame):
         '''Contructor'''
         super().__init__(
             master,
-            fg_color = "#1650CC",
+            fg_color = BLUE,
             width = 310,
             corner_radius = 0
         )
-        
+
         CTkFrame( #Padding
             self,
-            fg_color = "#1650CC",
+            fg_color = BLUE,
             width = 310,
             height= 40,
             corner_radius = 0
         ).pack(side = "top")
 
         title_frame = TitleFrame(self)
-        title_frame.pack(side = "top")
-        menu_button = CTkImage(light_image=Image.open("Assets/Menu.png"))
+        title_frame.pack(side = "top",fill = "y")
 
 class TitleFrame(CTkFrame):
     '''Menu Title Frame Class'''
     def __init__(self,master):
         super().__init__(
             master,
-            fg_color = "#1600CC",
+            fg_color = BLUE,
             width = 270,
-            height = 50,
+            height = 59,
             corner_radius = 0
         )
+        self.pack_propagate(0)
 
-def main():
+        titlemenu = CTkFrame(self, fg_color = BLUE)
+        titlemenu.pack(side = "top")
+
+        menu_image = CTkImage(light_image=Image.open("Assets/Menu.png"),size = (40,40))
+        menu_label = CTkLabel(titlemenu, image = menu_image,text = "")
+        menu_label.pack(side = "left")
+
+        title_label =  CTkLabel(
+            titlemenu,
+            text = "ENCRYPT|MINDS",
+            text_color = DARK_BLUE,
+            font = APP_FONT,
+            width = 310,
+            height = 10,
+            fg_color = WHITE,
+            corner_radius = 10
+        )
+        title_label.pack(side = "left", padx = (10,0))
+
+        line = CTkFrame(self, height = 2, width = 258, fg_color = LIGHT_BLUE)
+        line.pack(side = "top",fill = "both",pady = (10,0))
+
+def run():
     '''Driver Code'''
     global APP_FONT
     app = App()
-    # bar = CTkFrame(
-    #     app,
-    #     fg_color = "#1650CC",
-    #     width = 310,
-    #     height = 768
-    #     )
-
-
-    # label = CTkLabel(
-    #     bar,
-    #     text = "ENCRYPT|MINDS",
-    #     font = APP_FONT,
-    #     width = 309,
-    #     height = 10
-    #     )
-
-    # bar.pack(side = "left",fill = "both")
-    # top_padding.pack(side = "top")
-    # label.pack(side = "top")
     app.mainloop()
 if __name__ == "__main__":
-    main()
+    run()
